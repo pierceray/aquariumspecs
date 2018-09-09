56b32827b583822acb0501dcd558a2f78349c7f7
Ray Pierce <ray@digitalpierce.com>
2018-09-09 12:43:22 -0500

    feat: add css and react eslint plugin

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-export default () => <div>Welcome to next.js!</div>;
+import css from "../styles/main.scss";
+import React from "react";
+
+const Component = () => {
+    return <h1 className={css.mainHeader}>Aquarium Specs</h1>;
+};
+
+export default Component;
